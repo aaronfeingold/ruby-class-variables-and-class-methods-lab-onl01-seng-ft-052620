3fe30e45b0fedf8b748f58c6534f38644f55d09a
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-08 20:38:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,47 +1,114 @@
-class Song  
+require 'pry'
+
+class Song
   
-  attr_reader :name, :artist, :genre 
-  
-  @@count = 0
+  @@count = 0 
   @@artists = []
   @@genres = []
   
-  def initialize(name, artist, genre)
-    @name = name 
+  
+  def initialize (name, artist, genre)
+    @name = name
     @artist = artist 
-    @genre = genre
+    @genre = genre 
     @@count += 1 
-    @@artists << artist 
+    @@artists << @artist 
     @@genres << genre 
   end 
   
-  def self.count
-    @@count
+  def name
+    @name 
+  end
+  
+  def artist= (artist) 
+    @artist = artist
   end 
   
-  def self.artists
-    @@artists.uniq 
+  def artist 
+    @artist
   end 
   
-  def self.genres
-    @@genres.uniq
-  end  
+  def genre
+    @genre 
+  end 
   
-  def self.genre_count
-    genre_hash = {}
+  
     
-    self.genres.each do |genre|
-      genre_hash[genre] = @@genres.count {|g| g == genre }
-    end 
-    genre_hash 
-  end 
   
-  def self.artist_count
-    artist_hash = {}
+  
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Song  
+  
+#   attr_reader :name, :artist, :genre 
+  
+#   @@count = 0
+#   @@artists = []
+#   @@genres = []
+  
+#   def initialize(name, artist, genre)
+#     @name = name 
+#     @artist = artist 
+#     @genre = genre
+#     @@count += 1 
+#     @@artists << artist 
+#     @@genres << genre 
+#   end 
+  
+#   def self.count
+#     @@count
+#   end 
+  
+#   def self.artists
+#     @@artists.uniq 
+#   end 
+  
+#   def self.genres
+#     @@genres.uniq
+#   end  
+  
+#   def self.genre_count
+#     genre_hash = {}
     
-    self.artists.each do |artist|
-      artist_hash[artist] = @@artists.count {|a| a == artist }
-    end 
-    artist_hash  
-  end 
-end  
+#     self.genres.each do |genre|
+#       genre_hash[genre] = @@genres.count {|g| g == genre }
+#     end 
+#     genre_hash 
+#   end 
+  
+#   def self.artist_count
+#     artist_hash = {}
+    
+#     self.artists.each do |artist|
+#       artist_hash[artist] = @@artists.count {|a| a == artist }
+#     end 
+#     artist_hash  
+#   end 
+# end  
